51425d4593b8fb9c881a778d1e71e4d87d2bc9b4
leonel mazzan <leonelmazzan@gmail.com>
2021-08-09 15:55:33 -0300

    change proyect name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/plantillaRedux'),
+      dir: require('path').join(__dirname, './coverage/templateRedux'),
       subdir: '.',
       reporters: [
         { type: 'html' },
